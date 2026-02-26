710f033752d8fa0beeac4623dcca38c9b1d6f822
Pidguit <pidguit@gmail.com>
2026-02-26 14:05:52 -0500

    added a bunch of comments and reorganized code a little bit

--- player.gd
+++ player.gd
@@ -1,13 +1,11 @@
 extends CharacterBody2D
 
 
-const JUMP_VELOCITY = -210.0
-const TERMINAL_VELOCITY = 200
+const JUMP_VELOCITY = -210.0 # How fast and high character jumps
+const TERMINAL_VELOCITY = 200 # Max fall speed
 
-const SPEED = 95.0
-const ACCELERATION = 1300.0       # How fast the character accelerates
-const DECELERATION = 800.0       # How fast the character slows down
-
+const SPEED = 95.0 # Sets max horizontal speed
+const ACCELERATION = 1300.0 # How fast the character moves (x axis)
 var MaxAirJumps = 1
 var airjumpsavailable = 1
 
@@ -15,30 +13,29 @@ var last_direction = 1
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add the gravity and set max fall speed
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		if velocity.y > TERMINAL_VELOCITY:
 			velocity.y = TERMINAL_VELOCITY
-		
+	
+	# Resets double jump while on floor
 	if is_on_floor():
 		airjumpsavailable = MaxAirJumps
 		
-	# Handle jump.
+	# Handles jump and double jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
 	elif Input.is_action_just_pressed("ui_accept") and airjumpsavailable > 0:
 		velocity.y = JUMP_VELOCITY
 		airjumpsavailable = airjumpsavailable - 1
-		
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Detects what direction the character should be moving including null movement
 	var direction = 0
 	var left_pressed = Input.is_action_pressed("ui_left")
 	var right_pressed = Input.is_action_pressed("ui_right")
-
+	
 	if left_pressed and not right_pressed:
 		direction = -1
 		last_direction = -1
@@ -50,10 +47,8 @@ func _physics_process(delta: float) -> void:
 	else:
 		direction = 0
 	
-	# Now that direction is set, calculate target velocity
+	# Sets target velocity and actually moves	
 	var target_velocity_x = direction * SPEED
-
-	# Smooth movement
 	velocity.x = move_toward(velocity.x, target_velocity_x, ACCELERATION * delta)
 	
 	if velocity.x > 1:
