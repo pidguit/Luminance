1d5663ea973fc09df83c22fcddf73528e56831a8
Pidguit <pidguit@gmail.com>
2026-02-27 02:33:39 -0500

    just added some comments

--- player.gd
+++ player.gd
@@ -18,7 +18,6 @@ const WallJumpDuration = .15
 const NeutralWallJumpDuration = .10
 const WallJumpSpeed = 150
 const NeutralWallJumpSpeed = 100
-
 var walljumptimer = 0
 var walldirection = 0
 var walljumpoverride = false
@@ -26,13 +25,13 @@ var neutralwalljump = false
 
 # Sliding Variables
 var is_sliding = false
-var slide_timer = 0.25
-var slide_duration = .25
+const slide_duration = 0.25
+var slide_timer = 0.25 # This counts backwards, don't change it lol
 var slide_speed = 250
 var slide_jump_speed = .85
 
 func _physics_process(delta: float) -> void:
-	# direction the player is facing (put at top just in case for later)
+	# direction the player is facing (put at top just in case for later cause there's a lot of things that use this)
 	var direction = 0
 	
 	# Add the gravity and set max fall speed
@@ -52,7 +51,7 @@ func _physics_process(delta: float) -> void:
 		start_slide()
 		
 	if is_sliding == true:
-		slide_duration -= delta
+		slide_timer -= delta
 		if Input.is_action_just_pressed("jump"):
 			velocity.x *= slide_jump_speed
 			end_slide()
@@ -60,10 +59,11 @@ func _physics_process(delta: float) -> void:
 	if slide_duration <= 0:
 		end_slide()
 		
-	# Handles jump and double jump
+	# Handles jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
+	
+	# Handles Double Jump
 	elif Input.is_action_just_pressed("jump") and airjumpsavailable > 0 and not is_on_wall():
 		velocity.y = JUMP_VELOCITY
 		airjumpsavailable -= 1
@@ -85,14 +85,14 @@ func _physics_process(delta: float) -> void:
 	else:
 		direction = 0
 		
-	# Walking target velocity
+	# Walking target velocity, this is the default
 	var target_velocity_x = direction * SPEED
 	
-	# Get's wall direction
+	# Get's wall direction (for wall jump)
 	if is_on_wall():
 		walldirection = get_wall_normal().x
 		
-	# Initializes Wall Jump
+	# Activates a Wall Jump
 	if not is_on_floor() and is_on_wall() and Input.is_action_just_pressed("jump"):
 		walljumptimer = 0
 		walljumpoverride = true
@@ -102,7 +102,7 @@ func _physics_process(delta: float) -> void:
 		else:
 			neutralwalljump = false
 
-	# Changes target velocity for wall jumps
+	# Adds the velocity for wall jumps
 	if walljumpoverride == true:
 		walljumptimer += delta
 		
@@ -121,17 +121,16 @@ func _physics_process(delta: float) -> void:
 			walljumptimer = 0
 
 	# This actually moves the character (moves towards target velocity with acceleration)
-	#velocity.x = move_toward(velocity.x, target_velocity_x, ACCELERATION * delta)
-
 	if is_on_floor():
-		velocity.x = move_toward(velocity.x, target_velocity_x, ACCELERATION * delta)
+		velocity.x = move_toward(velocity.x, target_velocity_x, ACCELERATION * delta) # Walking  Around (default)
 	elif direction != 0 and sign(velocity.x) == sign(direction) and abs(velocity.x) > SPEED and walljumpoverride == false:
-		pass
+		pass # If flying air with higher than default speed, keeps it if moving in same direciton
 	elif direction == 0 and abs(velocity.x) > SPEED and walljumpoverride == false:
-		velocity.x = move_toward(velocity.x, target_velocity_x, (ACCELERATION/3) * delta)
+		velocity.x = move_toward(velocity.x, target_velocity_x, (ACCELERATION/3) * delta) # decelerates if has too much speed and tries to go opposite direction
 	else:
-		velocity.x = move_toward(velocity.x, target_velocity_x, (ACCELERATION) * delta)
+		velocity.x = move_toward(velocity.x, target_velocity_x, (ACCELERATION) * delta) # if you don't have too much speed, it goes back to default
 	
+	# Sliding down wall Physics
 	if is_on_wall():
 		var wall_dir = get_wall_normal().x
 		if (wall_dir > 0 and direction < 0) or (wall_dir < 0 and direction > 0):
@@ -144,22 +143,21 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.flip_h = false
 	elif velocity.x < -1:
 		$AnimatedSprite2D.flip_h = true
-		
-			# Wall Jump Detection
-			
+
 	move_and_slide()
-	# snaps character to pixels?	
+	# snaps character to pixels (don't  really understand this still tbh)
 	$AnimatedSprite2D.position = position.snapped(Vector2(1,1)) - position
 
+# Activates a slide
 func start_slide():
 	rotation_degrees = -90 * last_direction
 	is_sliding = true
-	slide_duration = slide_timer
+	slide_timer = slide_duration
 	var direction = last_direction
 	velocity.x = slide_speed * direction
 	
-	
+# Ends a slide
 func end_slide():
 	is_sliding = false
 	rotation_degrees = 0
-	airjumpsavailable += 1
+	airjumpsavailable += 1 # Just because I'm temporarly rotating the model, when jump cancels a slide, it uses double jump so giving it back
